e61585775d6f975da46051bdbba7f9c5747773c3
Div <sh.kim.x928@gmail.com>
2023-10-06 05:48:11 +0900

    add meta tag script

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -47,6 +47,10 @@ export default function Document() {
           as="font"
           type="font/woff2"
         />
+        <meta
+          name="ahrefs-site-verification"
+          content="bce11dd1e79258121a73a137633566152ab1d60a8584185afcc2f880797effa5"
+        />
         <script
           defer
           async
